293120cd6a29343943a9de391933657046ae9585
kinhl <kinhl@LOSTLOVE>
2024-05-26 19:28:44 +0700

    Add project files.

---
+++ Web1/api.aspx.cs
@@ -0,0 +1,77 @@
+﻿using SuatAn;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace web1
+{
+    public partial class api : System.Web.UI.Page
+    {
+        public class TeamStanding
+        {
+            public string rank { get; set; }
+            public string title { get; set; }
+            public string artist { get; set; }
+            public string listener { get; set; }
+
+
+        }
+
+        void top(string action)
+        {
+            try
+            {
+                // Kết nối đến cơ sở dữ liệu
+                SqlServer db = new SqlServer();
+
+                // Gọi stored procedure GetTopTracks từ cơ sở dữ liệu
+                SqlCommand cm = db.GetCmd("GetTopTracks");
+
+                // Thực thi truy vấn và nhận kết quả
+                string jsonString = (string)db.Scalar(cm);
+
+                // Chuyển đổi chuỗi JSON thành danh sách các đối tượng
+                List<TeamStanding> teamStats = JsonConvert.DeserializeObject<List<TeamStanding>>(jsonString);
+
+                // Chuyển đổi danh sách các đối tượng thành chuỗi JSON
+                string teamStatsJson = JsonConvert.SerializeObject(teamStats);
+
+                // Trả về kết quả cho client
+                this.Response.Write(teamStatsJson);
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi ở đây, ví dụ:
+                this.Response.Write("An error occurred: " + ex.Message);
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Lấy action từ request
+                string action = Request["action"];
+
+                switch (action)
+                {
+                    case "GetTopTracks":
+                        top(action);
+                        break;
+                    default:
+                        // Handle unknown action
+                        this.Response.Write("Unknown action.");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi ở đây, ví dụ:
+                this.Response.Write("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Web1/topset/mycode.js
@@ -0,0 +1,49 @@
+﻿$(document).ready(function () {
+    $.post("api.aspx", {
+        action: "GetTopTracks"
+    }, function (data) {
+        // Xử lý dữ liệu JSON nhận được
+        console.log(data); // Ví dụ: in ra console
+
+        // Chuyển đổi dữ liệu JSON thành mảng các đối tượng
+        var topTracks = JSON.parse(data);
+
+        // Tạo bảng HTML
+        var tableHtml = `
+            <table>
+                <thead>
+                    <tr>
+                        <th>Rank</th>
+                        <th>Name</th>
+                        <th>Artist</th>
+                        <th>Listeners</th>
+                    </tr>
+                </thead>
+                <tbody>
+        `;
+
+        // Lặp qua mỗi phần tử trong mảng dữ liệu
+        topTracks.forEach((item, index) => {
+            tableHtml += `
+                <tr>
+                    <td>${item.rank}</td>
+                    <td>${item.name}</td>
+                    <td>${item.artist}</td>
+                    <td>${item.listeners}</td>
+                </tr>
+            `;
+        });
+
+        tableHtml += `
+                </tbody>
+            </table>
+        `;
+
+        // Đưa bảng HTML vào một div có id là "table-container"
+        $('#table-container').html(tableHtml);
+
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        console.error("Request failed: " + textStatus + ", " + errorThrown);
+        $('#table-container').html("An error occurred while loading data.");
+    });
+});
